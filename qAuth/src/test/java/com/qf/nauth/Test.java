0e0c745bc508d7131f614a55d1daa9c4924252fd
senwu <senwu@quarkfinance.com>
2017-12-13 20:33:34 +0800

    2

--- qAuth/src/test/java/com/qf/nauth/Test.java
+++ qAuth/src/test/java/com/qf/nauth/Test.java
@@ -20,7 +20,7 @@ public class Test {
 		System.out.println("sixteen!");
 		System.out.println("fourteen!");
 		System.out.println("test!");
-		System.out.println("啛啛喳喳11!");
+		System.out.println("啛啛喳喳33!");
 		System.out.println("seventeen!");
 	}
 
