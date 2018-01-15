1aee204096171e28974e44de5342ed3e37e3ee82
senwu <senwu@quarkfinance.com>
2018-01-15 11:52:00 +0800

    Revert "test"
    This reverts commit 296ec3b44e7c45de29bb426751309491dcf97dd9.

--- qAuth/src/test/java/com/qf/nauth/Test.java
+++ qAuth/src/test/java/com/qf/nauth/Test.java
@@ -1,13 +1,11 @@
 package com.qf.nauth;
 
-import java.math.BigDecimal;
-
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		/*System.out.println("first!");
+		System.out.println("first!");
 		System.out.println("second!");
 		System.out.println("third!");
 		System.out.println("fourth!");
@@ -25,17 +23,7 @@ public class Test {
 		System.out.println("啛啛喳喳33!");
 		System.out.println("啛啛喳喳22!");
 		System.out.println("啛啛喳喳44!");
-		System.out.println("seventeen!");*/
-		BigDecimal pmtAmtMoth = new BigDecimal(6);
-		if(pmtAmtMoth.compareTo(new BigDecimal(7))==0){
-			System.out.println("相等");
-		}else if(pmtAmtMoth.compareTo(new BigDecimal(7))==1){
-			System.out.println("大于");
-		}else if(pmtAmtMoth.compareTo(new BigDecimal(7))==-1){
-			System.out.println("小于");
-		}else {
-			System.out.println("异常");
-		}
+		System.out.println("seventeen!");
 	}
 
 }
